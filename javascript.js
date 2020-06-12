b9ee967cc4eca87130aab5e6196a020fd62c4864
MicahWhiting <65786657+MicahWhiting@users.noreply.github.com>
2020-06-12 13:39:34 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,7 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var navToggle = document.getElementById('top-nav-toggle');
 
-    navToggle.addEventListener('click', toggleNav);
+    if (navToggle) {
+        navToggle.addEventListener('click', toggleNav);
+    }
+
+
+    var fNameNode = document.getElementById('fname');
+
+    if (fNameNode) {
+        var urlParams = new URLSearchParams(window.location.search),
+            getfName = urlParams.get('fname'),
+            getlName = urlParams.get('lname'),
+            fullName = getfName + ' ' + getlName;
+        fNameNode.textContent = fullName;
+    }
+
+    /*  This JavaScript looks for inputs named 'fname' and 'lname', copies the text within these textboxes, and pastes them into elements with an id equaling 'fname'.  */
 
 
 });
@@ -34,7 +49,7 @@ function validateEmail() {
     }
 }
 
-/*  The 'function' defines the JavaScript task and 'validateEmail' gives the function a name. The 'document.forms tells the JavaScript to look for forms within the HTML document. The '["signup"]' tells the JavaScript the name of the specific form it is supposed to monitor, and the ["emailtext"]' informs the JavaScript the name of the specific input it is supposed to monitor. The 'if (x =="") tells the JavaScript that if no text is provided within the emailtext input, the JavaScript should display the alert text. The 'alert("You need to include your email address.")' tells the JavaScript what text it should display in the alert message. The 'return false' prevents the form from being submitted if the alert is triggered. These parameters are true in the other JavaScript functions below except that the names of the inputs that the JavaScript is supposed to monitor are different as is the text that is to be displayed within each alert message.   */
+/*  The 'function' defines the JavaScript task and the 'validateEmail' gives the function a name. The 'document.forms tells the JavaScript to look for forms within the HTML document. The '["signup"]' tells the JavaScript the name of the specific form it is supposed to monitor, and the ["emailtext"]' informs the JavaScript the name of the specific input within the form it is supposed to monitor. The 'if (x =="") tells the JavaScript that if no text is provided within the emailtext input the JavaScript should display the alert text. The 'alert("You need to include your email address.")' tells the JavaScript what text it should display in the alert message. The 'return false' prevents the form from being submitted if the alert is triggered. These parameters are true in the other JavaScript functions below except that the names of the inputs that the JavaScript is supposed to monitor are different as is the text that is to be displayed within each alert message.   */
 
 function validateFirst() {
     var x = document.forms["signup"]["firsttext"].value;
